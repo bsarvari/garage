e63864ced72b16c3f2aaa00998d97ce1956661f7
Balazs Sarvari <balazs.sarvari@gmail.com>
2016-09-26 16:11:57 +0200

    Implementing cookie based local storage with far future expiration as a fallback mechanism for older user agents with no localStorage API support

--- app/components/Utils.js
+++ app/components/Utils.js
@@ -10,13 +10,79 @@
  * An interface to a persistent store backed either by localStorage or cookies if the former one is not supported.
  * TODO cookie backed storage
  */
+
+function _setCookie(key, valueStr, expirationInDays) {
+  var d = new Date();
+  d.setTime(d.getTime() + (expirationInDays*24*60*60*1000));
+  var expires = "expires="+d.toUTCString();
+  document.cookie = key + "=" + valueStr + "; " + expires;
+}
+
+function _clearCookie(key) {
+  if(key){
+    document.cookie = `${key}=; expires=Thu, 01 Jan 1970 00:00:00 UTC`;
+  }
+}
+
+function _getCookie(key) {
+  var name = key + "=";
+  var ca = document.cookie.split(';');
+  for(var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
+function _getLocalStorage() {
+  try {
+    var storage = window['localStorage'],
+      x = '__storage_test__';
+    storage.setItem(x, x);
+    storage.removeItem(x);
+    return storage;
+  }
+  catch(e) {
+    return false;
+  }
+}
+
+/**
+ * Either localStorage if supported by the UA or a simple cookie-backed implementation
+ */
+let _store = _getLocalStorage();
+if(!_store){
+  _store = {
+    getItem(key){
+      let itemStr = _getCookie(key);
+      if(itemStr){
+        return JSON.parse(itemStr);
+      }
+    },
+
+    setItem(key, valueObj){
+      if(key){
+        _setCookie(key, valueObj === undefined ? undefined: JSON.stringify(valueObj), 10*365);
+      }
+    }
+  };
+}
+
+/**
+ * Public interface for client-side storage
+ */
 let store = {
   set(key, valueObj){
-    localStorage.setItem(key, valueObj === undefined ? undefined: JSON.stringify(valueObj));
+    _store.setItem(key, valueObj === undefined ? undefined: JSON.stringify(valueObj));
   },
 
   get(key){
-    let itemStr = localStorage.getItem(key);
+    let itemStr = _store.getItem(key);
     if(itemStr){
       return JSON.parse(itemStr);
     }
